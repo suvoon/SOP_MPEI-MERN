eb85e715092bf637cdce240a9553f00e75f221b7
suvoon <epavlov.suvoon@gmail.com>
2023-03-13 18:24:01 +0300

    forum page visuals

--- client/src/pages/ForumsPage/ForumsPage.jsx
+++ client/src/pages/ForumsPage/ForumsPage.jsx
@@ -1,11 +1,15 @@
 import './style.css'
+import { useState } from 'react'
 
 export const ForumsPage = () => {
 
+    const [dropdown, setDropdown] = useState(false);
+
     return (
         <>
-            <div className="search-block">
+            <form className="search-block">
                 <div className="mglass-container">
+
                     <button type="button">
                         <img
                             src="/images/mglass-favicon.png"
@@ -17,16 +21,16 @@ export const ForumsPage = () => {
                     type="text"
                     placeholder='Ключевые слова'
                 />
-            </div>
+            </form>
             <nav className="filters-block">
-                <div className="filters__category">
-                    <span>Категория</span>
-                    <div className="category__dropdown">
+                <div className={`filters__category ${dropdown ? 'filters__category_active' : ''}`}>
+                    <button type='button' onClick={() => { setDropdown(!dropdown) }}>Категория</button>
+                    <div className={`category__dropdown ${dropdown ? 'category__dropdown_visible' : ''}`}>
                         <ul>
-                            <li>Важное</li>
-                            <li>Заметка</li>
-                            <li>Вопрос</li>
-                            <li>Обсуждение</li>
+                            <li><button type='button'>Важное</button></li>
+                            <li><button type='button'>Заметка</button></li>
+                            <li><button type='button'>Вопрос</button></li>
+                            <li><button type='button'>Обсуждение</button></li>
                         </ul>
                     </div>
                 </div>
@@ -36,16 +40,21 @@ export const ForumsPage = () => {
                 </ul>
             </nav>
             <table className="forums-table">
-                <tr className='forums-table__header'>
-                    <th>Тема</th>
-                    <th>Категория</th>
-                    <th>Ответы</th>
-                </tr>
-                <tr className='forums-table__row'>
-                    <td>Правила форумов</td>
-                    <td>Важное</td>
-                    <td>77</td>
-                </tr>
+                <thead>
+                    <tr className='forums-table__header'>
+                        <th>Тема</th>
+                        <th>Категория</th>
+                        <th>Ответы</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr className='forums-table__row'>
+                        <td>Правила форумов</td>
+                        <td>Важное</td>
+                        <td>77</td>
+                    </tr>
+                </tbody>
+
             </table>
         </>
     )
